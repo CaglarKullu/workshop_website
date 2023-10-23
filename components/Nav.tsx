f7c002e0c1cc8c18f7340dab7fdaa58fd1a72986
Caglar Kullu <caglarkullu@gmail.com>
2023-10-23 17:40:49 +0100

    add: breadcrumb

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,12 +11,12 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/ui/dropdown-menu"
-
+import NextBreadcrumb from './NextBreadcrumb'
 
 const Nav = () => {
   return (
     <nav>
-<div className="navbar bg-background flex-between w-full mb-16 pt-3">
+<div className="navbar bg-background flex-between w-full pt-3">
   <div className="flex-1">
   <Link className=" normal-case text-xl text-primary"  href={'/'}> Altolye Nova</Link>
   <div className=" justify-evenly space-x-5 md:space-x-2 pl-20" >
@@ -29,7 +29,6 @@ const Nav = () => {
   <div className="sm:flex hidden">
     <ul className="menu menu-horizontal px-1 ">
       <li>      <Link className='normal-case text-primary' href={'/search-page'}>Search Page</Link> </li>
-
       <li>
         <details>
           <summary className='normal-case text-primary'>
@@ -44,20 +43,29 @@ const Nav = () => {
     </ul>
   </div>
   {/* Mobile Navigation */}
-  <div className="sm:hidden flex relative text-primary">
+  <div className="sm:hidden flex relative text-primary menu menu-horizonta">
   <DropdownMenu>
   <DropdownMenuTrigger>Profile Icon</DropdownMenuTrigger>
   <DropdownMenuContent >
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuLabel >My Account</DropdownMenuLabel>
     <DropdownMenuSeparator />
-    <DropdownMenuItem>Search Events</DropdownMenuItem>
-    <DropdownMenuItem>My Favorites</DropdownMenuItem>
+    <DropdownMenuItem >Search Events</DropdownMenuItem>
+    <DropdownMenuItem >My Favorites</DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
 
   </div>
-</div>
 
+</div>
+<div className='bg-secondary h-1 flex-initial'></div>
+<NextBreadcrumb
+          homeElement={'Home'}
+          separator={">"}
+          activeClasses='text-primary'
+          containerClasses='flex py-5' 
+          listClasses='hover:underline mx-2'
+          capitalizeLinks
+        />
     </nav>
   )
 }
