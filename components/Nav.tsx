e882f733ac7749b74296db5ac3268d1c41b43256
Caglar Kullu <caglarkullu@gmail.com>
2023-10-23 17:05:49 +0100

    fix: nav component style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,30 +3,58 @@
 
 import Link from 'next/link'
 import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/ui/dropdown-menu"
+
 
 const Nav = () => {
   return (
     <nav>
-<div className="navbar bg-primary">
+<div className="navbar bg-background flex-between w-full mb-16 pt-3">
   <div className="flex-1">
-  <Link className=" normal-case text-xl text-secondary"  href={'/'}> Altolye Nova</Link>
+  <Link className=" normal-case text-xl text-primary"  href={'/'}> Altolye Nova</Link>
+  <div className=" justify-evenly space-x-5 md:space-x-2 pl-20" >
+    <Link href={''}>Categories</Link>
+    <Link href={''}>Something</Link>
+    <Link href={''}>Something Else</Link>
   </div>
-  <div className="flex-none">
+  </div>
+  {/* Desktop Navigation */}
+  <div className="sm:flex hidden">
     <ul className="menu menu-horizontal px-1 ">
-      <li>      <Link className='normal-case text-secondary' href={'/search-page'}>Search Page</Link> </li>
+      <li>      <Link className='normal-case text-primary' href={'/search-page'}>Search Page</Link> </li>
 
       <li>
         <details>
-          <summary className='normal-case text-secondary'>
+          <summary className='normal-case text-primary'>
             Parent
           </summary>
-          <ul className="p-2 bg-base-100 text-secondary">
+          <ul className="p-2 bg-base-100 text-primary">
             <li><a>Link 1</a></li>
             <li><a>Link 2</a></li>
           </ul>
         </details>
       </li>
     </ul>
+  </div>
+  {/* Mobile Navigation */}
+  <div className="sm:hidden flex relative text-primary">
+  <DropdownMenu>
+  <DropdownMenuTrigger>Profile Icon</DropdownMenuTrigger>
+  <DropdownMenuContent >
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Search Events</DropdownMenuItem>
+    <DropdownMenuItem>My Favorites</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
   </div>
 </div>
 
